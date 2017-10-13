059f15c4598eab119895491af4b9067fa21ebfce
Fawill <fawill@gmail.com>
2017-10-12 20:08:01 -0400

    add fx punch

---
+++ Assets/Scripts/PlayerFX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerFX : MonoBehaviour {
+
+	public GameObject hit;
+
+	public  void Punch(){
+		
+		hit.SetActive (true);
+
+
+	}
+
+
+	public void DisablePunch(){
+
+		hit.SetActive (false);
+	}
+
+}
