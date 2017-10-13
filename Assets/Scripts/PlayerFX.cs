bb173dd297a198ae23ddbab470e6a1862c1de477
Fawill <fawill@gmail.com>
2017-10-12 21:29:02 -0400

    add grab and block fx

--- Assets/Scripts/PlayerFX.cs
+++ Assets/Scripts/PlayerFX.cs
@@ -5,18 +5,42 @@ using UnityEngine;
 public class PlayerFX : MonoBehaviour {
 
 	public GameObject hit;
+	public GameObject parry;
+	public GameObject hold;
 
 	public  void Punch(){
 		
 		hit.SetActive (true);
-
-
 	}
 
 
 	public void DisablePunch(){
 
 		hit.SetActive (false);
+	}
+
+
+	public  void Block(){
+
+		parry.SetActive (true);
+	}
+
+
+	public void DisableBlock(){
+
+		parry.SetActive (false);
+	}
+
+
+	public  void Grab(){
+
+		hold.SetActive (true);
+	}
+
+
+	public void DisableGrab(){
+
+		hold.SetActive (false);
 	}
 
 }
